34ef5026149b4ab4fd82d5fbf9539e2828c73745
mrkaneryasar <144124777+mrkaneryasar@users.noreply.github.com>
2023-09-05 12:43:00 +0000

    Solution Project Source code
    Solution Project Source code

---
+++ solution/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int remainingAttempts = 5;
+
+        try
+        {
+            Console.Write("Platonun sağ üst koordinatlarını girin : ");
+            string[] boundaries = Console.ReadLine().Split(' ');
+            int platoX = int.Parse(boundaries[0]);
+            int platoY = int.Parse(boundaries[1]);
+
+            while (remainingAttempts > 0)
+            {
+                Console.WriteLine("Şimdi Rover bilgilerini girin Çıkmak için 'E' tuşuna basınız.");
+                string input = Console.ReadLine();
+
+                if (input.ToLower() == "e")
+                    break;
+
+                string[] roverInfo = input.Split(' ');
+                if (roverInfo.Length != 3)
+                {
+                    Console.WriteLine("Geçersiz rover bilgisi, Tekrar deneyin.");
+                    continue;
+                }
+
+                int roverX = int.Parse(roverInfo[0]);
+                int roverY = int.Parse(roverInfo[1]);
+                char roverMoveInfo = char.Parse(roverInfo[2].ToUpper());
+
+                Console.Write("Rover komutlarını (örn:LMLMLMLMM) giriniz : ");
+                string commands = Console.ReadLine();
+
+                RoverManager.MoveRover(ref roverX, ref roverY, ref roverMoveInfo, platoX, platoY, commands);
+
+                // Son konum bilgisi
+                Console.WriteLine($"Son pozisyon: {roverX} {roverY} {roverMoveInfo}");
+
+                remainingAttempts--;
+
+                Console.WriteLine($"Kalan Hareket Hakkınız: {remainingAttempts} ");
+            }
+
+            if (remainingAttempts == 0)
+            {
+                Console.WriteLine("Hakkınız bitti!");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ana Hata: {ex.Message}");
+        }
+    }
+
+    // Diğer yardımcı methodları da RoverManager sınıfına taşıyabilirsiniz.
+}
---
+++ solution/rovercontrol.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+public class RoverManager
+{
+    public static void MoveRover(ref int x, ref int y, ref char direction, int platoX, int platoY, string commands)
+    {
+        foreach (char command in commands)
+        {
+            try
+            {
+                if (command == 'L')
+                {
+                    //  sola döndür
+                    direction = TurnLeft(direction);
+                }
+                else if (command == 'R')
+                {
+                    //  sağa döndür
+                    direction = TurnRight(direction);
+                }
+                else if (command == 'M')
+                {
+                    ActionRover(ref x, ref y, direction, platoX, platoY);
+                    // Rover'ı ileri hareket ettir
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Hata: {ex.Message}");
+            }
+        }
+    }
+
+
+
+    static char TurnLeft(char direction)
+    {
+        switch (direction)
+        {
+            case 'N':
+                return 'W';
+            case 'W':
+                return 'S';
+            case 'S':
+                return 'E';
+            case 'E':
+                return 'N';
+            default:
+                return direction;
+        }
+    }
+
+    static char TurnRight(char direction)
+    {
+        switch (direction)
+        {
+            case 'N':
+                return 'E';
+            case 'E':
+                return 'S';
+            case 'S':
+                return 'W';
+            case 'W':
+                return 'N';
+            default:
+                return direction;
+        }
+    }
+
+    static void ActionRover(ref int x, ref int y, char direction, int platoX, int platoY)
+    {
+        switch (direction)
+        {
+            case 'N':
+                if (y < platoY)
+                    y++;
+                break;
+            case 'E':
+                if (x < platoX)
+                    x++;
+                break;
+            case 'S':
+                if (y > 0)
+                    y--;
+                break;
+            case 'W':
+                if (x > 0)
+                    x--;
+                break;
+        }
+    }
+
+
+    // Diğer yardımcı methodları da bu sınıfa taşıyabilirsiniz.
+}
